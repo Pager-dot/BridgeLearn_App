f43f40cf06cc5a9cb7615b6ddf66ff292a310a35
Sonu Verma <rameshrv980@gmail.com>
2025-02-13 00:47:17 +0530

    Newton Laws

--- app/src/main/java/pratheekv39/bridgelearn/io/MainActivity.kt
+++ app/src/main/java/pratheekv39/bridgelearn/io/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import pratheekv39.bridgelearn.io.navigation.NavGraph
 import pratheekv39.bridgelearn.io.theme.HomeTheme
 import pratheekv39.bridgelearn.io.theme.ThemeViewModel
+import pratheekv39.bridgelearn.io.ui.simulations.NewtonsLawsSimulation
 
 class MainActivity : ComponentActivity() {
 
